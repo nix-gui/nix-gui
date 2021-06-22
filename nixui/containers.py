2270fa7569677a9618195e3ac9f8aad76161cac1
andrew <andrew>
2021-06-22 13:45:16 -0400

    remove dead code

--- nixui/containers.py
+++ nixui/containers.py
@@ -51,33 +51,3 @@ class Tree:
 
     def get_leaf(self):
         return self.tree[LEAF] if LEAF in self.tree else {}
-
-
-
-
-
-# decorator to wrap original __init__
-def nested_dataclass(*args, **kwargs):
-
-    def wrapper(check_class):
-
-        # passing class to investigate
-        check_class = dataclass(check_class, **kwargs)
-        o_init = check_class.__init__
-
-        def __init__(self, *args, **kwargs):
-
-            for name, value in kwargs.items():
-
-                # getting field type
-                ft = check_class.__annotations__.get(name, None)
-
-                if is_dataclass(ft) and isinstance(value, dict):
-                    obj = ft(**value)
-                    kwargs[name]= obj
-                o_init(self, *args, **kwargs)
-        check_class.__init__=__init__
-
-        return check_class
-
-    return wrapper(args[0]) if args else wrapper
