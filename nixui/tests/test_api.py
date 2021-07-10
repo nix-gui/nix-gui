46f988b4c50df81ceee0cf79cac3758f6c919219
Adrian Parvin Ouano <adrianparvino@gmail.com>
2021-07-11 00:29:31 +0800

    Set up test_api.py

--- nixui/tests/test_api.py
+++ nixui/tests/test_api.py
@@ -8,6 +8,5 @@ SAMPLES_PATH = 'tests/sample'
 
 @pytest.mark.datafiles(SAMPLES_PATH)
 def test_get_option_data():
-    return True  # TODO: remove when evaluation doesn't require `nix-build`
-    os.environ['CONFIGURATION_PATH'] = os.path.join(SAMPLES_PATH, 'configuration.nix')
+    os.environ['CONFIGURATION_PATH'] = os.path.abspath(os.path.join(SAMPLES_PATH, 'configuration.nix'))
     assert api.get_option_data()
