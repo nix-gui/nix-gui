eb0bed589e9fbeca7c8744ea61275eabfd786fb1
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2021-07-31 04:10:00 +0800

    Move common functions to a separate library

--- nixui/nix/get_modules_defined_attrs.nix
+++ nixui/nix/get_modules_defined_attrs.nix
@@ -1,20 +1,12 @@
 module_path:
 let
-  lib = import <nixpkgs/lib>;
+  pkgs = import <nixpkgs> {};
+  inherit (pkgs) lib;
+
+  inherit (import ./lib.nix { inherit pkgs; }) recursiveIntersect evalModuleStub;
+
   nixos = import <nixpkgs/nixos> {configuration={};};
 
-  # config-path = ./nixui/tests/sample/configuration.nix;
-
-  config = builtins.removeAttrs (import module_path {config = {}; pkgs = import <nixpkgs> {}; lib = import <nixpkgs/lib>;}) ["imports"];
-
-  recursiveIntersect = xs: ys:
-    builtins.mapAttrs (k: ys':
-      let
-        xs' = xs."${k}";
-      in
-        if (xs'._type or "") == "option"
-        then { inherit (xs') loc; } // { position = builtins.unsafeGetAttrPos k ys; }
-        else (recursiveIntersect xs' ys')
-    ) ys;
+  config = builtins.removeAttrs (evalModuleStub module_path) ["imports"];
 in
 lib.collect (x: x ? loc) (recursiveIntersect nixos.options config)
