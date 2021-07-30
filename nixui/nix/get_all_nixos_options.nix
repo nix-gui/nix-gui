7000433bb9149fb03d277dd86ba9a09c70ae42e3
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2021-07-31 02:32:55 +0800

    Do not rely on =nixos-config= existing

--- nixui/nix/get_all_nixos_options.nix
+++ nixui/nix/get_all_nixos_options.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs/nixos> {};
+with import <nixpkgs/nixos> {configuration={};};
 builtins.mapAttrs
   (n: v: builtins.removeAttrs v ["default" "declarations"])
   (pkgs.nixosOptionsDoc { inherit options; }).optionsNix
