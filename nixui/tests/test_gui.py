cbd4853a426e0fe089b0c3c577173202cf63817f
andrew <andrew>
2021-09-23 13:36:56 -0400

    clean up test_gui

--- nixui/tests/test_gui.py
+++ nixui/tests/test_gui.py
@@ -1,28 +1,12 @@
 import pytest
 
-import json
-import os
-import random
-
-from PyQt5 import QtWidgets
-
-from nixui.graphics import main_window
 from nixui.options.attribute import Attribute
-from nixui import state_model
 
 
-SAMPLES_PATH = 'tests/sample'
+@pytest.mark.slow
+def test_integration_load_option_fields(nix_gui_main_window):
+    nav_interface = nix_gui_main_window.centralWidget()
+    statemodel = nix_gui_main_window.statemodel
 
-
-@pytest.mark.datafiles(SAMPLES_PATH)
-def test_integration_load_option_fields(qtbot):
-    os.environ['CONFIGURATION_PATH'] = os.path.abspath(os.path.join(SAMPLES_PATH, 'configuration.nix'))
-    os.environ['USE_DISKCACHE'] = json.dumps(False)
-
-    statemodel = state_model.StateModel()
-
-    nix_gui = main_window.NixGuiMainWindow(statemodel)
-    nav_interface = nix_gui.centralWidget()
-
-    for attr, _ in random.sample(list(statemodel.option_tree.iter_attribute_data()), 1000):
+    for i, (attr, _) in enumerate(statemodel.option_tree.iter_attribute_data()):
         nav_interface.set_option_path(attr)
