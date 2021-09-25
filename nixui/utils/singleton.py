dddb3e8baabb906534d4749e7c08d2c3daa457e0
andrew <andrew>
2021-09-24 22:56:28 -0400

    make singletons "json-hashable"

--- nixui/utils/singleton.py
+++ nixui/utils/singleton.py
@@ -1,6 +1,9 @@
+import dataclasses
+
+
+@dataclasses.dataclass(frozen=True, unsafe_hash=True)
 class Singleton:
-    def __init__(self, name):
-        self.name = name
+    name: str
 
     def __eq__(self, other):
         return (
@@ -10,3 +13,6 @@ class Singleton:
 
     def __repr__(self):
         return f'Singleton("{self.name}")'
+
+    def toJSON(self):
+        return f'SINGLETON {self.name}'
